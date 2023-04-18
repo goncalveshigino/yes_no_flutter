5d45eb784e0f9eff600debf5ebbaf9dc1698b371
goncalveshigino <goncalveshigino20@gmail.com>
2023-04-18 22:44:18 +0100

    show message

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -5,7 +5,7 @@ import 'package:yes_no_app/domain/entities/message_entities.dart';
 
 class ChatProvider extends ChangeNotifier {
 
-  List<MessageEntitie> message = [
+  List<MessageEntitie> messageList = [
     MessageEntitie(text: 'Ola Amor', fromWho: FromWho.me),
     MessageEntitie(text: 'Estas em casa?', fromWho: FromWho.me)
   ];
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message_entities.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
@@ -28,6 +31,8 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(
@@ -37,10 +42,11 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-
-                itemCount: 100,
+                itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
-                  return (index % 2 == 0)
+                  final message = chatProvider.messageList[index];
+
+                  return (message.fromWho == FromWho.hers)
                       ? const HerMessageBubble()
                       : const MyMessageBubble();
                 },
