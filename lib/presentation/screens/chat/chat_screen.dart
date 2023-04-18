4e613a44c52f8085b65c8ff20b4347df1f23df06
goncalveshigino <goncalveshigino20@gmail.com>
2023-04-18 10:57:53 +0100

    bubble message

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -35,7 +36,7 @@ class _ChatView extends StatelessWidget {
             Expanded(child: ListView.builder(
               itemCount: 100,
               itemBuilder: (context, index) {
-              return Text('Indece: $index');
+              return MyMessageBubble();
             })),
 
             Text('Ola Mundo')
